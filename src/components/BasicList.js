65e5c6977b50ada6fb8e936b59eaf53f0683f722
Christian Arana <coanelmejor@gmail.com>
2022-01-26 10:40:08 -0600

    Exploring how to add jsDocs to Components

--- src/components/BasicList.js
+++ src/components/BasicList.js
@@ -5,13 +5,24 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function BasicSelect(props) {
-  const [age, setAge] = React.useState(props.initialValue);
+/**
+ * Component for showing details of the user.
+ *
+ * @component
+ * @example
+ * const age = 21
+ * const name = 'Jitendra Nirnejak'
+ * return (
+ *   <User age={age} name={name} />
+ * )
+ */
+export default function BasicSelect({initialValue = String, onChange = Function, elements = [Object]}) {
+  const [age, setAge] = React.useState(initialValue);
 
   const handleChange = (event) => {
     setAge(event.target.value);
     console.log('BasicSelect: handleChange: event.target.value: ', event.target.value);
-    props.onChange(event)
+    onChange(event)
   };
 
   return (
@@ -26,7 +37,7 @@ export default function BasicSelect(props) {
           onChange={handleChange}
         >
           {
-            props.elements.map(
+            elements.map(
               (element) => (
                 <MenuItem key={element.id} value={element.id}>
                   {element.name}
