4575ffffad6b0e200725af86b0125f2b541cac19
Christian Arana <coanelmejor@gmail.com>
2022-01-27 09:10:16 -0600

    Changed Test for home

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Chromeless/i);
+  const linkElement = screen.getByText(/Disable UI/i);
   expect(linkElement).toBeInTheDocument();
 });
